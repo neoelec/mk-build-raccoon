2a7dc33fdeb80a8d2112b10deaa00df541117033
YOUNGJIN JOO <neoelec@gmail.com>
2026-08-02 13:02:54 +0900

    refactor(gcc_avr): eliminate parse-time subshells and check toolchain origin
    - Replace $(shell dirname) with GNU Make built-in path functions
    - Preserve user-defined CC and CXX using origin check
    
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- gcc_avr.mk
+++ gcc_avr.mk
@@ -1,37 +1,37 @@
 # SPDX-License-Identifier: GPL-2.0+
 # Copyright (c) 2024 YOUNGJIN JOO (neoelec@gmail.com)
 
-GCC_AVR_MK_FILE		:= $(realpath $(lastword $(MAKEFILE_LIST)))
-GCC_AVR_MK_DIR		:= $(shell dirname $(GCC_AVR_MK_FILE))
+GCC_AVR_MK_FILE		:= $(abspath $(lastword $(MAKEFILE_LIST)))
+GCC_AVR_MK_DIR		:= $(patsubst %/,%,$(dir $(GCC_AVR_MK_FILE)))
 
 CROSS_COMPILE		?= avr-
 
+ifeq ($(origin CC),default)
 CC			:= $(CROSS_COMPILE)gcc
+endif
+CC			?= $(CROSS_COMPILE)gcc
+
+ifeq ($(origin CXX),default)
 CXX			:= $(CROSS_COMPILE)g++
-OBJCOPY			:= $(CROSS_COMPILE)objcopy
-OBJDUMP			:= $(CROSS_COMPILE)objdump
-SIZE			:= $(CROSS_COMPILE)size
-STRIP			:= $(CROSS_COMPILE)strip
-NM			:= $(CROSS_COMPILE)nm
+endif
+CXX			?= $(CROSS_COMPILE)g++
+
+OBJCOPY			?= $(CROSS_COMPILE)objcopy
+OBJDUMP			?= $(CROSS_COMPILE)objdump
+SIZE			?= $(CROSS_COMPILE)size
+STRIP			?= $(CROSS_COMPILE)strip
+NM			?= $(CROSS_COMPILE)nm
 
 # MCU name
 MCU			?= atmega128
 
 # Processor frequency.
-#     This will define a symbol, F_CPU, in all source code files equal to the
-#     processor frequency. You can then use this symbol in your source code to
-#     calculate timings. Do NOT tack on a 'UL' at the end, this will be done
-#     automatically to create a 32-bit value in your source code.
 F_CPU			?= 16000000
 
 # Output format. (can be srec, ihex, binary)
 FORMAT			?= ihex
 
 # Compiler flag to set the C Standard level.
-#     c89   = "ANSI" C
-#     gnu89 = c89 plus GCC extensions
-#     c99   = ISO C99 standard (not yet fully implemented)
-#     gnu99 = c99 plus GCC extensions
 CSTANDARD		?= -std=gnu17
 CXXSTANDARD		?= -std=gnu++17
 
@@ -39,38 +39,16 @@ CXXSTANDARD		?= -std=gnu++17
 CDEFS			+= -mmcu=$(MCU) -DF_CPU=$(F_CPU)UL
 
 #---------------- Compiler Options ----------------
-#  -g*:          generate debugging information
-#  -O*:          optimization level
-#  -f...:        tuning, see GCC manual and avr-libc documentation
-#  -Wall...:     warning level
-#  -Wa,...:      tell GCC to pass this to the assembler.
-#    -adhlns...: create assembler listing
 CPPFLAGS		+= -funsigned-char
 CPPFLAGS		+= -funsigned-bitfields
 CPPFLAGS		+= -fpack-struct
 CPPFLAGS		+= -fshort-enums
 
 #---------------- Assembler Options ----------------
-#  -Wa,...:   tell GCC to pass this to the assembler.
-#  -ahlms:    create listing
-#  -gstabs:   have the assembler create line number information; note that
-#             for use in COFF files, additional information about filenames
-#             and function names needs to be present in the assembler source
-#             files -- see avr-libc docs [FIXME: not yet described there]
-#  -listing-cont-lines: Sets the maximum number of continuation lines of hex
-#       dump that will be displayed for a given single line of source input.
 ASFLAGS			+= -x assembler-with-cpp
 ASFLAGS			+= -Wa,-gstabs,--listing-cont-lines=100
 
 #---------------- External Memory Options ----------------
-
-# 64 KB of external RAM, starting after internal RAM (ATmega128!),
-# used for variables (.data/.bss) and heap (malloc()).
-#EXTMEMOPTS		:= -Wl,--section-start,.data=0x801100,--defsym=__heap_end=0x80ffff
-
-# 64 KB of external RAM, starting after internal RAM (ATmega128!),
-# only used for heap (malloc()).
-#EXTMEMOPTS		:= -Wl,--defsym=__heap_start=0x801100,--defsym=__heap_end=0x80ffff
 EXTMEMOPTS		?=
 
 LDFLAGS			+= $(EXTMEMOPTS)
