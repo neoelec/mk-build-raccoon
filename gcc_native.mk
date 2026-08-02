470e740119476961dcd8e243f74ba4f1d9ae6be2
YOUNGJIN JOO <neoelec@gmail.com>
2026-08-02 13:02:59 +0900

    refactor(gcc_native): eliminate parse-time subshells and add directory guards
    - Replace $(shell dirname) with GNU Make built-in path functions
    - Add @mkdir -p $(dir $@) guard for copy rule
    - Add missing all and clean phony targets
    
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- gcc_native.mk
+++ gcc_native.mk
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0+
 # Copyright (c) 2024 YOUNGJIN JOO (neoelec@gmail.com)
 
-GCC_NATIVE_MK_FILE	:= $(realpath $(lastword $(MAKEFILE_LIST)))
-GCC_NATIVE_MK_DIR	:= $(shell dirname $(GCC_NATIVE_MK_FILE))
+GCC_NATIVE_MK_FILE	:= $(abspath $(lastword $(MAKEFILE_LIST)))
+GCC_NATIVE_MK_DIR	:= $(patsubst %/,%,$(dir $(GCC_NATIVE_MK_FILE)))
 
 include $(GCC_NATIVE_MK_DIR)/mk/base_cc.mk
 include $(GCC_NATIVE_MK_DIR)/dbg/gdb.mk
@@ -10,6 +10,7 @@ include $(GCC_NATIVE_MK_DIR)/dbg/gdb.mk
 all: $(OUTPUT)
 
 $(OUTPUT): $(ELF_FILE)
+	@mkdir -p $(dir $@)
 	@$(COPY) $< $@
 	@$(STRIP) $@
 
@@ -21,4 +22,4 @@ clean: clean_native
 clean_native:
 	$(REMOVE) $(OUTPUT)
 
-.PHONY: run clean_native
+.PHONY: all run clean clean_native
