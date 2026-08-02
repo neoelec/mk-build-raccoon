4ba8015f546cdd2f09f50b955db727cd2ee696ab
YOUNGJIN JOO <neoelec@gmail.com>
2026-08-02 13:02:50 +0900

    refactor(gcc_at91): eliminate parse-time subshells and check toolchain origin
    - Replace $(shell dirname) with GNU Make built-in path functions
    - Preserve user-defined CC and CXX using origin check
    - Add condition check before running JLINK_CHIP uppercase shell call
    
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- gcc_at91.mk
+++ gcc_at91.mk
@@ -1,18 +1,26 @@
 # SPDX-License-Identifier: GPL-2.0+
 # Copyright (c) 2024 YOUNGJIN JOO (neoelec@gmail.com)
 
-GCC_AT91_MK_FILE	:= $(realpath $(lastword $(MAKEFILE_LIST)))
-GCC_AT91_MK_DIR		:= $(shell dirname $(GCC_AT91_MK_FILE))
+GCC_AT91_MK_FILE	:= $(abspath $(lastword $(MAKEFILE_LIST)))
+GCC_AT91_MK_DIR		:= $(patsubst %/,%,$(dir $(GCC_AT91_MK_FILE)))
 
 CROSS_COMPILE		?= arm-none-eabi-
 
+ifeq ($(origin CC),default)
 CC			:= $(CROSS_COMPILE)gcc
+endif
+CC			?= $(CROSS_COMPILE)gcc
+
+ifeq ($(origin CXX),default)
 CXX			:= $(CROSS_COMPILE)g++
-OBJCOPY			:= $(CROSS_COMPILE)objcopy
-OBJDUMP			:= $(CROSS_COMPILE)objdump
-SIZE			:= $(CROSS_COMPILE)size
-STRIP			:= $(CROSS_COMPILE)strip
-NM			:= $(CROSS_COMPILE)nm
+endif
+CXX			?= $(CROSS_COMPILE)g++
+
+OBJCOPY			?= $(CROSS_COMPILE)objcopy
+OBJDUMP			?= $(CROSS_COMPILE)objdump
+SIZE			?= $(CROSS_COMPILE)size
+STRIP			?= $(CROSS_COMPILE)strip
+NM			?= $(CROSS_COMPILE)nm
 
 # Chip & board used for compilation
 # (can be overriden by adding CHIP=chip and BOARD=board to the command-line)
@@ -32,26 +40,20 @@ include $(GCC_AT91_MK_DIR)/mk/at91/platform/$(PLATFORM).mk
 TRACE_LEVEL		?= 0
 
 # Output directories
-BINDIR			:= bin
-OBJDIR			:= obj
+BINDIR			?= bin
+OBJDIR			?= obj
 
 # Append OBJ and BIN directories to output filename
-OUTPUT			:= $(addprefix $(BINDIR)/, $(TARGET)-$(BOARD)-$(CHIP))
+OUTPUT			?= $(addprefix $(BINDIR)/, $(TARGET)-$(BOARD)-$(CHIP))
 
 VPATH			+= $(AT91LIB)/utility
 
 EXTRAINCDIRS		+= $(AT91LIB)
 
-CSRCS			+=
-ASRCS			+=
+CSRCS			?=
+ASRCS			?=
 
 #---------------- Compiler Options ----------------
-#  -g*:          generate debugging information
-#  -O*:          optimization level
-#  -f...:        tuning, see GCC manual
-#  -Wall...:     warning level
-#  -Wa,...:      tell GCC to pass this to the assembler.
-#    -adhlns...: create assembler listing
 CPPFLAGS		+= -D$(CHIP)
 CPPFLAGS		+= -DTRACE_LEVEL=$(TRACE_LEVEL)
 CPPFLAGS		+= -mlong-calls
@@ -59,20 +61,9 @@ CPPFLAGS		+= -ffunction-sections
 CPPFLAGS		+= --param=min-pagesize=0
 
 #---------------- Assembler Options ----------------
-#  -Wa,...:   tell GCC to pass this to the assembler.
-#  -ahlms:    create listing
-#  -gstabs:   have the assembler create line number information; note that
-#             for use in COFF files, additional information about filenames
-#             and function names needs to be present in the assembler source
-#             files [FIXME: not yet described there]
-#  -listing-cont-lines: Sets the maximum number of continuation lines of hex
-#       dump that will be displayed for a given single line of source input.
 ASFLAGS			+= -D__ASSEMBLY__
 
 #---------------- Linker Options ----------------
-#  -Wl,...:     tell GCC to pass this to linker.
-#    -Map:      create map file
-#    --cref:    add cross reference to  map file
 LDFLAGS			+= -T"$(LD_SCRIPT)"
 LDFLAGS			+= -nostartfiles
 LDFLAGS			+= -Wl,--gc-sections
@@ -81,7 +72,9 @@ LDFLAGS			+= -Wl,--start-group -lc -lm -Wl,--end-group
 
 include $(GCC_AT91_MK_DIR)/mk/base_cc.mk
 
+ifneq ($(strip $(CHIP)),)
 JLINK_CHIP		:= $(shell echo $(CHIP) | tr a-z A-Z)
 JFLASH_PRJ		:= $(GCC_AT91_MK_DIR)/mk/at91/jflash/$(CHIP).jflash
+endif
 
 include $(GCC_AT91_MK_DIR)/dbg/jlink.mk
